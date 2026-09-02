f50f29759e749b222ccf6c482cb0ec9d4eaaf365
romaina229 <nounonvenanromain@gmail.com>
2026-09-02 12:22:11 +0100

    md bailleur

--- src/pages/Donors.tsx
+++ src/pages/Donors.tsx
@@ -3,7 +3,7 @@ import { NavBar } from '../components/NavBar'
 import { useOrganization } from '../context/OrganizationContext'
 import { fetchDonors, createDonor, deleteDonor, type Donor } from '../services/donors'
 
-const DONOR_TYPES = ['bailleur_institutionnel', 'fondation', 'etat', 'particulier', 'autre']
+const DONOR_TYPES = ['Aailleur_Institutionnel', 'Fondation', 'Etat', 'Particulier', 'Autre']
 
 export default function Donors() {
   const { currentOrganization } = useOrganization()
